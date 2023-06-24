c458507ad4af0cca3d4a7e6f7dcce81a83ea3b71
maD-skillz <warneverchang3s@gmail.com>
2023-06-24 19:16:39 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/PersonsApplication.java
+++ src/main/java/ru/job4j/PersonsApplication.java
@@ -1,16 +1,34 @@
 package ru.job4j;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class PersonsApplication {
+public class PersonsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonsApplication.class);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:db/dbchangeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsApplication.class, args);
-		System.out.println("Go to http://localhost:8080/person");
+		System.out.println("Go to http://localhost:8080/persons");
 	}
 
 	@Bean
